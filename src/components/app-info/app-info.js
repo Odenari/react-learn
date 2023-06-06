f88bace4733fdc621eb0e147f598bf0f3bc1194b
Odenari <odenari44@gmail.com>
2023-06-06 18:49:58 +0200

    finishing a simple layout

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,12 @@
+import './app-info.css';
+const AppInfo = () => {
+	return (
+		<div className="app-info">
+			<h1 className="app-title">Учет сотрудников компании "G"</h1>
+			<h2 className="app-subtitle">Общее число сотрудников: </h2>
+			<h2 className="bonus-list">Премию получат: </h2>
+		</div>
+	);
+};
+
+export default AppInfo;
